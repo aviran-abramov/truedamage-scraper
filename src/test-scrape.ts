b5c26dec445d96c1a0cf3386335a4610eb4db0bb
Aviran <aviranabramov@gmail.com>
2026-02-12 12:26:07 +0200

    feat: extract match team names

--- src/test-scrape.ts
+++ src/test-scrape.ts
@@ -9,9 +9,13 @@ import { chromium } from 'playwright';
     // Main content
     // Match page
     await page.goto("https://www.gosugamers.net/counterstrike/tournaments/62664-cct-season-3-european-series-16/matches/640993-prime-vs-hyperspirit");
-    await page.screenshot({ path: 'screenshot.png' });
-    await page.waitForTimeout(5000);
-    console.log(await page.title());
+
+    // Team Data
+    const teamData = page.locator('a[href*="/teams/"]');
+    const teamNames = teamData.locator('.MuiTypography-root.MuiTypography-p4');
+    const teamAName = await teamNames.first().textContent();
+    const teamBName = await teamNames.last().textContent();
+    console.log(`Team A: ${teamAName}, Team B: ${teamBName}`);
 
     // Finish
     await context.close();
