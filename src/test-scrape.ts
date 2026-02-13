6fa0d56f4e4059c135fa192338ae21f16f9253f8
Aviran <aviranabramov@gmail.com>
2026-02-13 20:11:09 +0200

    refactor: organize code for teams data

--- src/test-scrape.ts
+++ src/test-scrape.ts
@@ -10,12 +10,16 @@ import { chromium } from 'playwright';
     // Match page
     await page.goto("https://www.gosugamers.net/counterstrike/tournaments/62664-cct-season-3-european-series-16/matches/640993-prime-vs-hyperspirit");
 
-    // Team Data
-    const teamData = page.locator('a[href*="/teams/"]');
-    const teamNames = teamData.locator('.MuiTypography-root.MuiTypography-p4');
+
+    // Teams Data
+    const teamContainers = page.locator('a[href*="/teams/"]');
+    const teamNames = teamContainers.locator('.MuiTypography-root.MuiTypography-p4');
+
     const teamAName = await teamNames.first().textContent();
+    console.log(`Team A: ${teamAName}`); // Gentle Mates
+
     const teamBName = await teamNames.last().textContent();
-    console.log(`Team A: ${teamAName}, Team B: ${teamBName}`);
+    console.log(`Team B: ${teamBName}`);
 
     // Finish
     await context.close();
