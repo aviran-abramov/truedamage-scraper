6a9df56bebc7206ccd36c08b082fb8221782e0f6
Aviran Abramov <aviranabramov@gmail.com>
2026-03-05 15:49:26 +0200

    chore: add scrapeMatchLinks.ts with steps

---
+++ src/scrapeMatchLinks.ts
@@ -0,0 +1,6 @@
+// Goals
+// 1. Go to https://www.gosugamers.net/matches
+// 2. Extract links
+// 4. Get games with time left & time is less than 1 day
+// 5. Add rows to the Google SpreadSheet
+// 6. Go to the next page and repeat 4,5 if needed
