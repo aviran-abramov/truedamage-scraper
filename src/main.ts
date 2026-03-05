3541349364c2d9e3491ed254cac9072a4b8803df
Aviran Abramov <aviranabramov@gmail.com>
2026-03-05 15:35:19 +0200

    chore: create main function and add scrapeMatch

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { chromium } from "playwright";
+import { scrapeMatch } from "./scrapeMatch";
 
 const launchPage = async (matchUrl: string) => {
   console.log("🟡 Connecting to Chromium browser...");
@@ -12,3 +13,11 @@ const launchPage = async (matchUrl: string) => {
 
   return { browser, context, page };
 };
+
+const matchUrl =
+  "https://www.gosugamers.net/dota2/tournaments/62605-fissure-universe-episode-8/matches/639158-aurora-gaming-vs-1w-team";
+
+async function main() {
+  await scrapeMatch(matchUrl);
+}
+main();
