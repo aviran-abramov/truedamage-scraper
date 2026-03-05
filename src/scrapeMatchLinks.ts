3998c3b1ff4c3c8e3853e33fedb5bf987b0f5445
Aviran Abramov <aviranabramov@gmail.com>
2026-03-05 19:01:17 +0200

    chore: add launchPage in scrapeMatchLinks

--- src/scrapeMatchLinks.ts
+++ src/scrapeMatchLinks.ts
@@ -1,6 +1,19 @@
+import { launchPage } from "./lib/browser";
+
 // Goals
 // 1. Go to https://www.gosugamers.net/matches
 // 2. Extract links
 // 4. Get games with time left & time is less than 1 day
 // 5. Add rows to the Google SpreadSheet
 // 6. Go to the next page and repeat 4,5 if needed
+
+const matchUrl = "https://www.gosugamers.net/matches";
+
+export async function scrapeMatchLinks() {
+  // Initialize
+  const { browser, context, page } = await launchPage(matchUrl);
+
+  // Finish
+  await context.close();
+  await browser.close();
+}
