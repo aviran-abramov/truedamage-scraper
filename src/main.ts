67b9099ec84818e4e0a6f6a01519e74ea97948be
Aviran <aviranabramov@gmail.com>
2026-03-13 21:56:19 +0200

    refactor(main): remove redundant matchUrl variable

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import scrapeMatch from "./scrape/match";
 import scrapeMatchLinks from "./scrape/matchLinks";
 
-const matchUrl =
-  "https://www.gosugamers.net/dota2/tournaments/62605-fissure-universe-episode-8/matches/639158-aurora-gaming-vs-1w-team";
-
 async function main() {
   const urls = await scrapeMatchLinks();
   const first = await scrapeMatch(urls[0]);
